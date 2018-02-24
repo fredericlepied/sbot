deb48c8c98779c3679ece09bcab5b3055cbe5546
Frederic Lepied <frederic.lepied@redhat.com>
2018-02-24 12:12:42 +0100

    autoupdate: allow to have not pushed changes

--- autoupdate.pl
+++ autoupdate.pl
@@ -10,13 +10,16 @@
 %% fact updater
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
+% working directory is dirty
 autoupdate_code(_) :-
     not(cmd("git diff --no-ext-diff --quiet --exit-code", [])),
     make,
     !.
 
+% working directory is clean, so update from remote with support for
+% local commits not pushed
 autoupdate_code(_) :-
-    cmd("git pull", []),
+    cmd("git fetch origin; git rebase origin/master", []),
     make.
 
 :- add_fact_updater(autoupdate:autoupdate_code).
