695ef575e747b20f58e137813a3bf411d20a9d1b
Yanis Guenane <yanis@guenane.org>
2018-03-23 09:51:36 +0100

    Record not understood sentence in db

--- admin.pl
+++ admin.pl
@@ -4,6 +4,7 @@
           [
           ]).
 
+:- use_module(world).
 :- use_module(discuss).
 :- use_module(utils).
 
@@ -11,12 +12,14 @@
 %% communication predicates
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-admin_answer(List, [_,Nick|_], Answer) :-
+admin_answer(["admin", "help"], [_, Nick|_],
+           ["Available commands:\n",
+            bold("kill"), ": kill botsito.\n",
+            bold("notunderstood list"), ": list all sentences bot did not unserstand.\n",
+            bold("notunderstood remove <sentence>"), ": remove sentence from the not understood sentence databas."
+           ]) :-
     config(admins, Admins),
-    member("admin", List),
-    member("help", List),
-    member(Nick, Admins),
-    format(string(Answer), "~n~w", ["kill: Kill Botsito"]).
+    member(Nick, Admins).
 
 admin_answer(List, _, Answer) :-
     member("admin", List),
@@ -34,6 +37,26 @@ admin_answer(["kill"], _, Answer) :-
 admin_answer(["admins"], _, Answer) :-
     config(admins, Admins),
     string_join(", ", Admins, Answer).
+
+admin_answer(["notunderstood", "list"], [_,Nick|_], Answer) :-
+    config(admins, Admins),
+    member(Nick, Admins),
+    findall(Str, get_longterm_fact(not_understood_sentence(Str)), Strs),
+    string_join("\n", Strs, Sentences),
+    format(string(Answer), "\n~w", [Sentences]).
+
+admin_answer(["notunderstood", "list"], _, Answer) :-
+    format(string(Answer), "~w", ["Not allowed"]).
+
+admin_answer(["notunderstood", "remove"|Sentence], [_,Nick|_], Answer) :-
+    config(admins, Admins),
+    member(Nick, Admins),
+    string_join(" ", Sentence, SentenceStr),
+    remove_longterm_fact(not_understood_sentence(SentenceStr)),
+    format(string(Answer), "~w removed from notunderstood sentence fact", [SentenceStr]).
+
+admin_answer(["notunderstood", "remove"|_], _, Answer) :-
+    format(string(Answer), "~w", ["Not allowed"]).
 
 :- add_answerer(admin:admin_answer).
 
