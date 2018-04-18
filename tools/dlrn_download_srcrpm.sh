936a8f999a16a361d914b2f4aa903169b8beb76b
Frederic Lepied <frederic.lepied@redhat.com>
2018-04-18 16:13:33 +0200

    tools/dlrn_download_srcrpm.sh: add a name prefix on disk for DLRN directories

--- tools/dlrn_download_srcrpm.sh
+++ tools/dlrn_download_srcrpm.sh
@@ -4,12 +4,13 @@ set -eu
 
 WSP=$1
 URL=$2
-DIR=$3
+NAME=$3
+DIR=$4
 
 set -x
 
-mkdir -p $WSP/$DIR
-cd $WSP/$DIR
+mkdir -p $WSP/$NAME/$DIR
+cd $WSP/$NAME/$DIR
 wget -q -r -np -nd -L -A '*.src.rpm' -A '*.cfg' -A 'copr.log' -A 'rpmbuild.log' $URL$DIR/
 rm -f child.cfg site-defaults.cfg
 ls *.cfg
